01e7e5ae9ae6fdfc70d99ee39fc9711849a079e6
nikhilbalne <nikhil.b@ecoihub.com>
2020-06-06 10:55:39 +0530

    Initial Commit

---
+++ OTPScreenAcceptsSingleDigit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OTPScreenAcceptsSingleDigit
+//
+//  Created by Nikhil Balne on 06/06/20.
+//  Copyright © 2020 Nikhil Balne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
