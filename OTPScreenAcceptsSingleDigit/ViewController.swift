a313b1324050374df9db5189879dcdd48c3efe70
nikhilbalne <nikhil.b@ecoihub.com>
2020-06-06 11:05:24 +0530

    Added code

--- OTPScreenAcceptsSingleDigit/ViewController.swift
+++ OTPScreenAcceptsSingleDigit/ViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var otpTextField1: UITextField!
+    @IBOutlet weak var otpTextField2: UITextField!
+    @IBOutlet weak var otpTextField3: UITextField!
+    @IBOutlet weak var otpTextField4: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        otpTextField1.addTarget(self, action: #selector(self.textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        otpTextField2.addTarget(self, action: #selector(self.textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        otpTextField3.addTarget(self, action: #selector(self.textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        otpTextField4.addTarget(self, action: #selector(self.textFieldDidChange(textField:)), for: UIControl.Event.editingChanged)
+        
     }
 
+    @objc func textFieldDidChange(textField: UITextField){
+        let text = textField.text
+        if  text?.count == 1 {
+            switch textField{
+            case otpTextField1:
+                otpTextField2.becomeFirstResponder()
+            case otpTextField2:
+                otpTextField3.becomeFirstResponder()
+            case otpTextField3:
+                otpTextField4.becomeFirstResponder()
+            case otpTextField4:
+                otpTextField4.resignFirstResponder()
+            default:
+                break
+            }
+        }
+        if  text?.count == 0 {
+            switch textField{
+            case otpTextField1:
+                otpTextField1.becomeFirstResponder()
+            case otpTextField2:
+                otpTextField1.becomeFirstResponder()
+            case otpTextField3:
+                otpTextField2.becomeFirstResponder()
+            case otpTextField4:
+                otpTextField3.becomeFirstResponder()
+            default:
+                break
+            }
+        }
+        else{
 
+        }
+    }
+    
 }
 
